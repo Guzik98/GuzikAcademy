e530e3df97fbaf8befde374a26426497ba18c591
Guzik98 <guzkac2@gmail.com>
2021-08-04 15:52:22 +0200

    my solution on how to fetch data and story in table

---
+++ index.js
@@ -0,0 +1,126 @@
+const companiesList = [];
+const usersList = [];
+
+const port = "3000";
+const usersAddress = "http://localhost:"+port+"/users";
+const companiesAddress = "http://localhost:"+port+"/companies"
+
+let container = document.querySelector("tbody");
+let table = ""
+
+window.onload = function () {
+
+    fetchUsers();
+
+    fetchCompany();
+
+    insertCompanyInTable();
+}
+
+function fetchUsers() {
+    fetch(usersAddress)
+        .then(res => res.json())
+        .then(res =>{
+            for( let i = 0; i<res.length; i++){
+                usersList.push({
+                    name: res[i].name,
+                    email: res[i].email,
+                    companyNumber: res[i].uris.company.slice(11)
+                });
+            }
+            console.log(usersList)
+        })
+}
+
+function fetchCompany() {
+    fetch(companiesAddress)
+        .then(res => res.json())
+
+        .then(res => {
+            for (let i = 0; i < res.length; i++) {
+                companiesList.push({
+                    companyNumber: res[i].name,
+                    users: []
+                });
+            }
+        })
+
+        .then(()=>{
+            ConnectUsersToCompany();
+        })
+
+        .then(()=>{
+            SortCompaniesByUsers();
+            console.log(companiesList)
+        })
+
+        .then(()=>{
+            insertCompanyInTable();
+        })
+}
+
+//Wstawiamy uzytkownika do tabeli users in company
+function ConnectUsersToCompany() {
+    for (let i = 0; i < usersList.length; i++) {
+        let companyNumber = usersList[i].companyNumber;
+        let companyUserName = usersList[i].name;
+        companiesList[companyNumber].users.push(companyUserName);
+    }
+}
+
+//Sortujemy firmy po przez uzytkownikow
+function SortCompaniesByUsers(){
+    companiesList.sort(function (a, b) {
+        return a.users.length - b.users.length;
+    })
+}
+
+//wstawiamy firmy do tabeli
+function insertCompanyInTable() {
+    let id = 0;
+    for( let i = 0; i <companiesList.length; i++) {
+
+        if (companiesList[i].users.length >= 1) {
+            id += 1;
+            table += `<tr onclick="showRow()" class="level-1">
+                                    <th scope="row">${id}</th>                                                              
+                                    <td>${companiesList[i].companyNumber}</td>
+                                    <td>${companiesList[i].users.length}</td>
+                                </tr>`
+            table += `<thead>
+                                        <tr class="level-2">
+                                            <th scope="col">#</th>
+                                            <th scope="col">User Name</th>
+                                            <th scope="col">Email</th>
+                                        </tr>
+                                    </thead>`
+            insertUsersInTable(i);
+        }
+    }
+    container.innerHTML = table
+}
+
+//wstawiamy uzytkownikow do tabeli
+function insertUsersInTable(i) {
+    for (let j = 0; j < companiesList[i].users.length; j++) {
+        let index = companiesList[i].users[j].slice(5)
+        table += `<tr class="level-2">
+                    <th scope="row">${j+1}</th>                                                              
+                    <td>${usersList[index].name}</td>
+                    <td>${usersList[index].email}</td>
+                  </tr>`
+    }
+}
+
+//funkcja do rozwijania tabeli
+function showRow() {
+    $(".level-1").on("click",function () {
+        console.log("click")
+        let target = $(this).nextUntil(".level-1");
+        $(target).toggle();
+    });
+}
+
+
+
+
