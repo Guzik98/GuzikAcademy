97bfdf4fbef3c06ba20e7d7bdd9572eac260ea6f
Guzik98 <guzkac2@gmail.com>
2021-08-06 15:14:20 +0200

    code changing from jquery toggle to dom

--- app/static/index.js
+++ app/static/index.js
@@ -9,11 +9,12 @@ const companiesAddress = url+"companies"
 const tbody = document.querySelector("tbody");
 let table = ""
 
-window.onload = function () {
+window.onload = async function () {
 
   fetchUsers();
 
   fetchCompany();
+
 }
 
 function fetchUsers() {
@@ -42,6 +43,7 @@ function fetchCompany() {
           users: []
         });
       }
+      console.log(companiesList)
     })
 
     .then(()=>{ConnectUsersToCompany();})
@@ -49,6 +51,28 @@ function fetchCompany() {
     .then(()=>{SortCompaniesByUsers();})
 
     .then(()=>{insertCompanyInTable();})
+
+    .then(()=> {
+      const mainTable = document.querySelectorAll(".level-one")
+      for (let i = 0; i < mainTable.length; i++) {
+        mainTable[i].addEventListener("click", function () {
+
+          let nextRow = this.nextElementSibling;
+          let className = nextRow.getAttribute("class");
+          while (className === "level-two") {
+
+            if(nextRow.style.display === "table-row"){
+              nextRow.style.display = "none";
+            }else{
+              nextRow.style.display = "table-row";
+            }
+
+            nextRow = nextRow.nextElementSibling;
+            className = nextRow.getAttribute("class");
+          }
+        })
+      }
+    })
 }
 
 //Wstawiamy uzytkownika do tabeli users in company
@@ -70,23 +94,22 @@ function SortCompaniesByUsers(){
 //wstawiamy firmy do tabeli
 function insertCompanyInTable() {
 
-  let id = 0;
+
   for(let i = 0; i <companiesList.length; i++) {
 
-    if (companiesList[i].users.length >= 1) {
-      id += 1;
-      table += `<tr onclick="showRow()" class="level-one">
-                                    <td>${id}</td>                                                              
+      table += `<tr class="level-one">
+                                    <td>${i+1}</td>                                                              
                                     <td>${companiesList[i].companyNumber}</td>
                                     <td>${companiesList[i].users.length}</td>
                                 </tr>`
-      table += `<thead >
-                                        <tr class="level-2">
+    if (companiesList[i].users.length >= 1) {
+      table += `<thead>
+                                        <tr class="level-two">
                                             <th class="th-level-two" scope="col">#</th>
                                             <th class="th-level-two" scope="col">User Name</th>
                                             <th class="th-level-two" scope="col">Email</th>
                                         </tr>
-                                    </thead>`
+                                  </thead> `
       insertUsersInTable(i);
     }
   }
@@ -97,20 +120,12 @@ function insertCompanyInTable() {
 function insertUsersInTable(i) {
   for (let j = 0; j < companiesList[i].users.length; j++) {
     const userIndex = companiesList[i].users[j].slice(5)
-    table += `<tr class="level-2">
+    table += `<tr class="level-two">
                     <td>${j+1}</td>                                                              
                     <td>${usersList[userIndex].name}</td>
                     <td>${usersList[userIndex].email}</td>
                   </tr>`
   }
-}
-
-//funkcja do rozwijania tabeli
-function showRow() {
-  $(".level-one").on("click",function () {
-    let target = $(this).nextUntil(".level-one");
-    $(target).toggle();
-  });
 }
 
 
