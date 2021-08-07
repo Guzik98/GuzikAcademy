fa559c417fe1f6d20214b76bb79c39775caef59f
Guzik98 <guzkac2@gmail.com>
2021-08-07 19:34:58 +0200

    code refactor

--- app/static/index.js
+++ app/static/index.js
@@ -7,43 +7,44 @@ const usersAddress = url+"users";
 const companiesAddress = url+"companies"
 
 const tbody = document.querySelector("tbody");
+
 let table = ""
-let fileNumber = 1;
+let fileNumber =1;
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  loadData().then(r =>{
-    console.log(r)});
+  loadData();
 })
 
-async  function loadData() {
-  let userData = fetch(usersAddress);
-  let companyData = fetch(companiesAddress);
-  Promise.all([userData,companyData])
-    .then((files) => {
-      files.forEach(file =>{
-        process(file.json())
+//pobieranie danych i parsowanie
+function loadData() {
+    let userData = fetch(usersAddress);
+    let companyData = fetch(companiesAddress);
+    Promise.all([userData,companyData])
+      .then((files) => {
+        files.forEach(file =>{
+          process(file.json())
+        })
       })
-    })
-    .catch(err => console.log(err)
-    )
+      .catch(err => alert("sprawdz czy serwer działa \n kod: " + err)
+      )
 }
 
+//procesowanie danych
 function process(prom) {
   prom.then(async (data) => {
+    console.log(data)
     if (fileNumber === 1) {
       storeUserDataInList(data)
     } else {
       await storeCompanyDataInList(data)
-      await ConnectUsersToCompany();
-      await SortCompaniesByUsers();
+      await connectUsersToCompany();
       await insertCompanyInTable();
-      await showRow()
     }
     fileNumber++;
   })
 }
 
+//segragacja danych firm
 function storeCompanyDataInList(data) {
   for (let i = 0; i < data.length; i++) {
     companiesList.push({
@@ -54,92 +55,89 @@ function storeCompanyDataInList(data) {
   console.log(companiesList)
 }
 
-  function storeUserDataInList(data) {
-    for (let i = 0; i < data.length; i++) {
-      usersList.push({
-        name: data[i].name,
-        email: data[i].email,
-        companyNumber: data[i].uris.company.slice(11)
-      });
-    }
-    console.log(usersList)
+//segragacja danych uzytkowników
+function storeUserDataInList(data) {
+  for (let i = 0; i < data.length; i++) {
+    usersList.push({
+      name: data[i].name,
+      email: data[i].email,
+      companyNumber: data[i].uris.company.slice(11)
+    });
   }
-
+  console.log(usersList)
+}
 
 //Wstawiamy uzytkownika do tabeli users in company
-  function ConnectUsersToCompany() {
-    for (let i = 0; i < usersList.length; i++) {
-      const companyNumber = usersList[i].companyNumber;
-      const companyUserName = usersList[i].name;
-      companiesList[companyNumber].users.push(companyUserName);
-    }
-    console.log(companiesList);
+function connectUsersToCompany() {
+  for (let i = 0; i < usersList.length; i++) {
+    const companyNumber = usersList[i].companyNumber;
+    const companyUserName = usersList[i].name;
+    companiesList[companyNumber].users.push(companyUserName);
   }
-
-//Sortujemy firmy po ilosc uzytkownikow
-  function SortCompaniesByUsers() {
-    companiesList.sort(function (a, b) {
-      return a.users.length - b.users.length;
-    })
-    console.log(companiesList);
-  }
+  //sortowanie po ilośc uzytkowników
+  companiesList.sort((a,b) =>{
+    return a.users.length - b.users.length;
+  })
+  console.log(companiesList);
+}
 
 //wstawiamy firmy do tabeli
-  function insertCompanyInTable() {
+function insertCompanyInTable() {
+  for (let i = 0; i < companiesList.length; i++) {
 
-
-    for (let i = 0; i < companiesList.length; i++) {
-
-      table += `<tr class="level-one">
-                                    <td>${i + 1}</td>                                                              
-                                    <td>${companiesList[i].companyNumber}</td>
-                                    <td>${companiesList[i].users.length}</td>
-                                </tr>`
-      if (companiesList[i].users.length >= 1) {
-        table += `<thead>
-                                        <tr class="level-two">
-                                            <th class="th-level-two" scope="col">#</th>
-                                            <th class="th-level-two" scope="col">User Name</th>
-                                            <th class="th-level-two" scope="col">Email</th>
-                                        </tr>
-                                  </thead> `
+    table += `<tr class="level-one">
+                <td>${i + 1}</td>                                                              
+                <td>${companiesList[i].companyNumber}</td>
+                <td>${companiesList[i].users.length}</td>
+              </tr>`
+    if (companiesList[i].users.length >= 1) {
+      table += `<thead>
+                    <tr class="level-two">
+                        <th class="th-level-two" scope="col">#</th>
+                        <th class="th-level-two" scope="col">User Name</th>
+                        <th class="th-level-two" scope="col">Email</th>
+                    </tr>
+                 </thead> `
         insertUsersInTable(i);
       }
     }
     tbody.innerHTML = table
+    showRow();
   }
 
 //wstawiamy uzytkownikow do tabeli
-  function insertUsersInTable(i) {
-    for (let j = 0; j < companiesList[i].users.length; j++) {
-      const userIndex = companiesList[i].users[j].slice(5)
-      table += `<tr class="level-two">
-                    <td>${j + 1}</td>                                                              
-                    <td>${usersList[userIndex].name}</td>
-                    <td>${usersList[userIndex].email}</td>
-                  </tr>`
-    }
+function insertUsersInTable(i) {
+  for (let j = 0; j < companiesList[i].users.length; j++) {
+    const userIndex = companiesList[i].users[j].slice(5)
+    table += `<tr class="level-two">
+                  <td>${j + 1}</td>                                                              
+                  <td>${usersList[userIndex].name}</td>
+                  <td>${usersList[userIndex].email}</td>
+             </tr>`
+  }
 }
 
 //rozwijanie tabeli
 function showRow (){
   const mainTable = document.querySelectorAll(".level-one")
   for (let i = 0; i < mainTable.length; i++) {
-    mainTable[i].addEventListener("click", function () {
+    mainTable[i].addEventListener("click",  function (){
       let nextRow = this.nextElementSibling;
       let className = nextRow.getAttribute("class");
-      while (className === "level-two") {
-
-        if (nextRow.style.display === "table-row") {
-          nextRow.style.display = "none";
-        } else {
-          nextRow.style.display = "table-row";
-        }
-
-        nextRow = nextRow.nextElementSibling;
-        className = nextRow.getAttribute("class");
-      }
+      toggle(nextRow,className)
     })
+  }
+}
+
+function toggle(nextRow, className) {
+  while (className === "level-two") {
+    if (nextRow.style.display === "table-row") {
+      nextRow.style.display = "none";
+    } else {
+      nextRow.style.display = "table-row";
+    }
+    nextRow = nextRow.nextElementSibling;
+    className = nextRow.getAttribute("class");
   }
 }
 
